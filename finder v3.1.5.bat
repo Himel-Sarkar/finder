b12f9a14db5667d06f1a7d434c76a79523f0f231
Himel Sarkar <57190235+Himel-Sarkar@users.noreply.github.com>
2021-06-20 18:08:50 +0600

    Update finder v3.1.5.bat

--- finder v3.1.5.bat
+++ finder v3.1.5.bat
@@ -55,6 +55,7 @@ cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs") && 
 
 pushd "%~dp0"
 
+mshta vbscript:Execute("msgbox ""[!] legal disclaimer: Usage of Finder Framework for attacking targets without prior mutual consent is illegal. It is the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program"":close")
 
 echo off
 :begin
