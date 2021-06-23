38612650ad335c9fd25f8a224828e87b7ef284cf
Himel Sarkar <57190235+Himel-Sarkar@users.noreply.github.com>
2021-06-23 15:21:01 +0600

    Update finder v3.1.5.bat

--- finder v3.1.5.bat
+++ finder v3.1.5.bat
@@ -3442,7 +3442,7 @@ echo  1. yandex
 echo  2. duckduckgo
 echo  3. yahoo
 echo  4. google
-echo  5. Free_Premium_Leeches (Premium link free downloader)
+echo  5. Free_Premium_Leeches (Premium link free downloader just like:nitroflare,rapidgator)
 echo  6. social-searcher
 echo  7. keywordshitter (Keyword Descover/Grabbing Combos)
 echo  8. reddit
